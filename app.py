949ce114e273e471500528b812f43304bdba559b
salmasoesmono <134788566+salmasoesmono@users.noreply.github.com>
2024-07-21 16:38:18 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,38 @@
 import streamlit as st
 import pandas as pd
-import exploration
-import preprocessing
-import model_training
-
-st.title("Insurance Data Analysis and Model Training")
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Load data
-data_path = 'insurance.csv'
-data = pd.read_csv(data_path)
+@st.cache
+def load_data():
+    return pd.read_csv('insurance.csv')
+
+data = load_data()
 
 # Exploration
-st.header("Exploration")
-if st.checkbox("Show raw data"):
+st.title('Exploratory Data Analysis')
+if st.checkbox('Show raw data'):
     st.write(data.head())
 
-exploration.main(data)
+# Plotting
+st.subheader('Histograms and Boxplots')
+numeric_cols = ['age', 'bmi', 'children', 'charges']
+for col in numeric_cols:
+    st.subheader(f'Histogram of {col}')
+    fig, ax = plt.subplots()
+    sns.histplot(data[col], bins=30, ax=ax)
+    st.pyplot(fig)
+    
+    st.subheader(f'Boxplot of {col}')
+    fig, ax = plt.subplots()
+    sns.boxplot(x=data[col], ax=ax)
+    st.pyplot(fig)
 
-# Preprocessing
-st.header("Preprocessing")
-preprocessed_data = preprocessing.main(data)
-st.write(preprocessed_data.head())
-
-# Model Training and Evaluation
-st.header("Model Training and Evaluation")
-train_and_evaluate.main(preprocessed_data)
+st.subheader('Countplots for Categorical Variables')
+categorical_cols = ['sex', 'smoker', 'region']
+for col in categorical_cols:
+    st.subheader(f'Countplot of {col}')
+    fig, ax = plt.subplots()
+    sns.countplot(x=col, data=data, ax=ax)
+    st.pyplot(fig)
